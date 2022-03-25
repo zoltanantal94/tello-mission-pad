0678e7a61e87e0ae910b98fc4c92133842ea6f86
Antal Zoltan <zoltan.antal.11@gmail.com>
2022-03-25 16:01:37 +0100

    Add alternative glob2loc_coord() function

--- fly7.py
+++ fly7.py
@@ -145,3 +145,48 @@ def glob2loc_coord(global_coordinate, pad_id):
         local_y = global_y - pad_coordinates[k_alt][1]
 
     return [local_x, local_y]
+
+
+def glob2loc_coord_alt(global_coordinate, pad_id):
+    """ Convert global coordinates to local coordinate
+        Arguments:
+            global_coordinate: [x,y] in meter
+            pad_id: 1~8
+    """
+    global_x = global_coordinate[0]
+    global_y = global_coordinate[1]
+    pad_coordinates = [
+        [[0.30, 1.50], 1],  # 1.1,  1
+        [[0.90, 1.50], 2],  # 1.2   2
+        [[1.50, 1.50], 3],  # 1.3   3
+        [[2.10, 1.50], 4],  # 1.4   4
+        [[0.30, 0.90], 3],  # 2.1   3
+        [[0.90, 0.90], 4],  # 2.2   4
+        [[1.50, 0.90], 5],  # 2.3   5
+        [[2.10, 0.90], 6],  # 2.4   6
+        [[0.30, 0.30], 5],  # 3.1   5
+        [[0.90, 0.30], 6],  # 3.2   6
+        [[1.50, 0.30], 7],  # 3.3   7
+        [[2.10, 0.30], 8]   # 3.4   8
+    ]
+    if pad_id == -1:
+        raise Exception("pad_id must be greater than zero.")
+    else:
+        possible_pads = []
+        # select pads by id
+        for p in pad_coordinates:
+            if p[1] == pad_id:
+                possible_pads.append(p[0])
+        # calculate distances
+        for p in possible_pads:
+            p.append(
+                math.sqrt(
+                    (global_x - p[0]) ** 2 + (global_y - p[1]) ** 2
+                )
+            )
+        # sort array by 3rd column, and return first row
+        selected_pad = sorted(possible_pads, key=lambda x: x[2])[0]
+        local_x = global_x - selected_pad[0]
+        local_y = global_y - selected_pad[1]
+
+    return [local_x, local_y]
